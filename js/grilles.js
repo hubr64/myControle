64cd998aaae5f6d10a55ef6b99a839ebadbf5c94
hubr.md@gmail.com <hubr.md@gmail.com>
2017-11-08 22:51:45 +0100

    Mise à jour de la grille

--- js/grilles.js
+++ js/grilles.js
@@ -184,5 +184,88 @@ var _grilles = {
 				}
 			}
 		}
-	}
+	},
+	"grille2016l":{
+		"titre":"Lycée",
+		"competences":{
+			"competence1":{
+				"titre":"S'approprier",
+				"couleur":"#C0584D",
+				"capacites":{
+					"App1":"Rechercher, extraire et organiser l’information utile",
+					"App2":"Adopter une attitude critique vis-à-vis de l’information",
+					"App3":"Questionner, identifier, formuler un problème",
+					"App4":"Reformuler",
+					"App5":"Identifier les risques",
+				}
+			},
+			"competence2":{
+				"titre":"Analyser",
+				"couleur":"#FAD46D",
+				"capacites":{
+					"Ana1":"Émettre une hypothèse",
+					"Ana2":"Identifier les paramètres qui influencent un phénomène, choisir les grandeurs à mesurer",
+					"Ana3":"Élaborer ou justifier un protocole",
+					"Ana4":"Proposer une méthode, un calcul, un outil adapté ; faire des essais (choisir, adapter une méthode, un protocole)",
+					"Ana5":"Proposer, décrire un modèle ; utiliser un modèle pour prévoir, décrire et expliquer",
+					"Ana6":"Percevoir la différence entre un modèle et la réalité, entre la réalité et une simulation",
+				}
+			},
+			"competence3":{
+				"titre":"Réaliser",
+				"couleur":"#6194C8",
+				"capacites":{
+					"Réa1":"Réaliser un montage à partir d’un schéma",
+					"Réa2":"Suivre un protocole donné",
+					"Réa3":"Utiliser, dans un contexte donné, le matériel à disposition",
+					"Réa4":"Savoir choisir, combiner et réaliser plusieurs actions",
+					"Réa5":"Effectuer un relevé de mesures",
+					"Réa6":"Schématiser, construire un graphique, un tableau, etc...",
+					"Réa7":"Exploiter une relation, un calcul littéral",
+					"Réa8":"Effectuer un calcul numérique, utiliser les symboles et les unités appropriés, utiliser la calculatrice",
+					"Réa9":"Reconnaître et utiliser la proportionnalité",
+					"Réa10":"Respecter les règles de sécurité, manipuler avec soin, veiller au rangement du plan de travail, etc...",
+				}
+			},
+			"competence4":{
+				"titre":"Valider",
+				"couleur":"#2DD16F",
+				"capacites":{
+					"Val1":"Estimer l’incertitude d’une mesure, faire un traitement statistique d’une série de mesures, etc...",
+					"Val2":"Interpréter des résultats, juger de la qualité d’une mesure, etc...",
+					"Val3":"Confronter le résultat au résultat attendu, mettre en relation, déduire",
+					"Val4":"Valider ou invalider une information, une hypothèse, etc...",
+				}
+			},
+			"competence5":{
+				"titre":"Communiquer",
+				"couleur":"#f39c00",
+				"capacites":{
+					"Com1":"Communiquer des résultats, rédiger une solution",
+					"Com2":"Exprimer un résultat (grandeur ─ unité ─ chiffres significatifs)",
+					"Com3":"Rendre compte à l’écrit ou à l’oral en utilisant un vocabulaire adapté",
+				}
+			},
+			"competence6":{
+				"titre":"Etre autonome, faire preuve d’initiative",
+				"couleur":"#BBB",
+				"capacites":{
+					"Aut1":"S’impliquer",
+					"Aut2":"Prendre des initiatives, anticiper, faire preuve de créativité",
+					"Aut3":"Travailler en autonomie",
+					"Aut4":"Travailler en équipe",
+				}
+			},
+			"competence7":{
+				"titre":"Mobiliser ses connaissances",
+				"couleur":"#6BD1D3",
+				"capacites":{
+					"Co1":"Connaître les notions scientifiques du programme",
+					"Co2":"Connaître  le vocabulaire approprié",
+					"Co3":"Connaître les symboles",
+					"Co4":"Connaître les unités",
+				}
+			},
+		},
+	},
 };
