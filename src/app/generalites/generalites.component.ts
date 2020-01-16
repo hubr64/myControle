6809f24663c188c29fec6ad26242e00a46cb7e49
u008039 <hubert.martin-deidier@safrangroup.com>
2020-01-16 16:59:43 +0100

    Reprise du module Généralités avec nouvelles fonctions Classe et Grille

--- src/app/generalites/generalites.component.ts
+++ src/app/generalites/generalites.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 import { DevoirService } from '../_services/devoir.service';
+import { GrilleService } from '../_services/grille.service';
+import { ClasseService } from '../_services/classe.service';
 import { MessageService } from '../_services/message.service';
 import { ConfigurationService } from '../_services/configuration.service';
+import { ModalConfirmGridComponent } from '../modal-confirm-grid/modal-confirm-grid.component';
+import { ModalConfirmClasseComponent } from '../modal-confirm-classe/modal-confirm-classe.component';
 
-import { NgbDateAdapter, NgbDateStruct, NgbDateNativeAdapter, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDateAdapter, NgbDateStruct, NgbDateNativeAdapter, NgbDateParserFormatter, NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 import { NgbDateCustomParserFormatter } from '../_helpers/dateformat';
 
 @Component({
@@ -26,11 +32,17 @@ export class GeneralitesComponent implements OnInit {
 
   constructor(
     private devoirService: DevoirService,
+    private grilleService: GrilleService,
+    private classeService: ClasseService,
     private messageService: MessageService,
+    private modalService: NgbModal,
+    config: NgbDropdownConfig,
     private configurationService: ConfigurationService) {
     this.notationModeNormal = this.configurationService.getValue('notationModeNormal');
     this.notationModeProportionnel = this.configurationService.getValue('notationModeProportionnel');
     this.notationModeRapporte = this.configurationService.getValue('notationModeRapporte');
+
+    config.container = 'body';
   }
 
   ngOnInit() {
@@ -54,6 +66,62 @@ export class GeneralitesComponent implements OnInit {
       this.notationModeNormalTitre = 'Mode de notation';
       this.notationModeNormalContent = 'Transformation de la note pour arriver à la note finale :';
     }
+  }
+
+  grilleChanged(newGrille) {
+    // No grille defined then can assigned a new one
+    if (this.devoirService.devoir.grille === null) {
+      this.devoirService.devoir.grille = newGrille;
+    } else {
+      // There was a grid defined and a new one is selected
+      if (this.grilleService.compareGrille(this.devoirService.devoir.grille, newGrille) === false) {
+
+        // Display modal window to ask user for confirmation
+        const modalRef = this.modalService.open(ModalConfirmGridComponent, { centered: true, scrollable: true });
+        // @ts-ignore: Provide it the required grille to apply (to display more information)
+        modalRef.componentInstance.grilleConfirm = newGrille;
+
+        // Manage answer of the user
+        modalRef.result.then((result) => {
+          this.devoirService.replaceGrille(newGrille);
+        }, (reason) => {
+          // Remove is cancelled
+          this.messageService.add('Changement de grille annulé', 'warning', 'USER');
+        });
+      }
+    }
+  }
+
+  toggleGrilleSelection() {
+    this.grilleService.showGrille(this.devoirService.devoir.grille, false);
+  }
+
+  classeChanged(newClasse) {
+    // No classe defined then can assigned a new one
+    if (this.devoirService.devoir.classe === null) {
+      this.devoirService.devoir.classe = newClasse;
+    } else {
+      // There was a classe defined and a new one is selected
+      if (this.classeService.compareClasse(this.devoirService.devoir.classe, newClasse) === false) {
+
+        // Display modal window to ask user for confirmation
+        const modalRef = this.modalService.open(ModalConfirmClasseComponent, { centered: true, scrollable: true });
+        // @ts-ignore: Provide it the required classe to apply (to display more information)
+        modalRef.componentInstance.classeConfirm = newClasse;
+
+        // Manage answer of the user
+        modalRef.result.then((result) => {
+          this.devoirService.replaceClasse(newClasse);
+        }, (reason) => {
+          // Remove is cancelled
+          this.messageService.add('Changement de classe annulé', 'warning', 'USER');
+        });
+      }
+    }
+  }
+
+  toggleClasseSelection() {
+    this.classeService.showClasse(this.devoirService.devoir.classe, false);
   }
 
   get today() {
