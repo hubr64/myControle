d9e411d5e6c5acb06d3f2e8b5d791df71f2b0d49
u008039 <hubert.martin-deidier@safrangroup.com>
2020-01-16 16:58:59 +0100

    Ajout d'une fonction de déplacement dans le devoir

---
+++ src/app/modal-move/modal-move.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { DevoirService } from '../_services/devoir.service';
+
+
+@Component({
+  selector: 'app-modal-move',
+  templateUrl: './modal-move.component.html',
+  styleUrls: ['./modal-move.component.sass']
+})
+export class ModalMoveComponent implements OnInit {
+
+  @Input() public item;
+  public itemType;
+  public selectedItem;
+  public selectedPosition;
+  public returnValue;
+
+  constructor(
+    private devoirService: DevoirService,
+    private modal: NgbActiveModal) {
+    this.item = null;
+    this.itemType = '';
+    this.selectedItem = null;
+    this.selectedPosition = null;
+    this.returnValue = {};
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.updateDetails();
+    });
+  }
+
+  updateDetails() {
+    if (this.item) {
+      this.itemType = this.item.constructor.name;
+    } else {
+      setTimeout(() => {
+        this.updateDetails();
+      }, 200);
+    }
+  }
+
+  selectItem(item, position) {
+    this.selectedItem = item;
+    this.selectedPosition = position;
+    this.returnValue = {
+      item: this.selectedItem,
+      position: this.selectedPosition
+    };
+  }
+
+}
+
