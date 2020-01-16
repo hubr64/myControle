d9bc8eb7551e00be7be3bbd66948c667b1b2d3cf
u008039 <hubert.martin-deidier@safrangroup.com>
2020-01-16 17:03:33 +0100

    Reprise design de la optionBox

--- src/app/devoir-edition-optionbox/devoir-edition-optionbox.component.ts
+++ src/app/devoir-edition-optionbox/devoir-edition-optionbox.component.ts
@@ -5,6 +5,7 @@ import { DevoirService } from '../_services/devoir.service';
 import { MessageService } from '../_services/message.service';
 import { ClipboardService } from '../_services/clipboard.service';
 import { ModalConfirmComponent } from '../modal-confirm/modal-confirm.component';
+import { ModalMoveComponent } from '../modal-move/modal-move.component';
 
 import { Exercice } from '../_models/exercice';
 import { Freetext } from '../_models/freetext';
@@ -100,6 +101,33 @@ export class DevoirEditionOptionboxComponent implements OnInit {
     }
   }
 
+  deleteConfirmItem(item) {
+    // If user confirms thus look for the item to be removed
+    for (const [indexExe, exercice] of this.devoirService.devoir.exercices.entries()) {
+      if (exercice.id === item.id) {
+        // Remove exercise if this the requested item
+        this.devoirService.devoir.exercices.splice(indexExe, 1);
+        break;
+      } else if (this.devoirService.devoir.exercices[indexExe].questions) {
+        for (const [indexQue, question] of this.devoirService.devoir.exercices[indexExe].questions.entries()) {
+          if (question.id === item.id) {
+            // Remove question if this the requested item
+            this.devoirService.devoir.exercices[indexExe].questions.splice(indexQue, 1);
+            break;
+          } else if (this.devoirService.devoir.exercices[indexExe].questions[indexQue].criteres) {
+            for (const [indexCri, critere] of this.devoirService.devoir.exercices[indexExe].questions[indexQue].criteres.entries()) {
+              // Remove critere if this the requested item
+              if (critere.id === item.id) {
+                this.devoirService.devoir.exercices[indexExe].questions[indexQue].criteres.splice(indexCri, 1);
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
   deleteItem(item) {
 
     // Display modal window to ask user for confirmation
@@ -109,35 +137,73 @@ export class DevoirEditionOptionboxComponent implements OnInit {
 
     // Manage answer of the user
     modalRef.result.then((result) => {
-      // If user confirms thus look for the item to be removed
-      for (const [indexExe, exercice] of this.devoirService.devoir.exercices.entries()) {
-        if (exercice.id === item.id) {
-          // Remove exercise if this the requested item
-          this.devoirService.devoir.exercices.splice(indexExe, 1);
-          break;
-        } else if (this.devoirService.devoir.exercices[indexExe].questions) {
-          for (const [indexQue, question] of this.devoirService.devoir.exercices[indexExe].questions.entries()) {
-            if (question.id === item.id) {
-              // Remove question if this the requested item
-              this.devoirService.devoir.exercices[indexExe].questions.splice(indexQue, 1);
-              break;
-            } else if (this.devoirService.devoir.exercices[indexExe].questions[indexQue].criteres) {
-              for (const [indexCri, critere] of this.devoirService.devoir.exercices[indexExe].questions[indexQue].criteres.entries()) {
-                // Remove critere if this the requested item
-                if (critere.id === item.id) {
-                  this.devoirService.devoir.exercices[indexExe].questions[indexQue].criteres.splice(indexCri, 1);
-                  break;
+      // Remove the item
+      this.deleteConfirmItem(item);
+      // Remove is now finished then display a confirmation
+      this.messageService.add('Suppression terminée', 'success', 'USER');
+    }, (reason) => {
+      // Remove is cancelled
+      this.messageService.add('Suppression annulée', 'warning', 'USER');
+    });
+  }
+
+  moveItem(item: any) {
+    // Display modal window to ask user for confirmation
+    const modalRef = this.modalService.open(ModalMoveComponent, { centered: true, size: 'xl', scrollable: true });
+    // @ts-ignore: Provide it the required item to delete (to display more information)
+    modalRef.componentInstance.item = item;
+
+    // Manage answer of the user
+    modalRef.result.then((result) => {
+
+      const selectedItem = result.item;
+      const selectedPosition = result.position;
+
+      // Directly remove without conformation
+      this.deleteConfirmItem(item);
+
+      // On ajoute tout à la fin
+      if (selectedItem === null && selectedPosition === 'end') {
+        this.devoirService.devoir.exercices.push(item);
+      } else {
+        // On ajoute pas tout à la fin
+        for (const [indexExe, exercice] of this.devoirService.devoir.exercices.entries()) {
+          if (exercice.id === selectedItem.id && selectedPosition === 'before') {
+            // On insère avant l'identifiant fourni
+            this.devoirService.devoir.exercices.splice(indexExe, 0, item);
+            break;
+          } else if (exercice.id === selectedItem.id && selectedPosition === 'end') {
+            // Si on veut ajouter à la fin de l'exercice
+            this.devoirService.devoir.exercices[indexExe].questions.push(item);
+            break;
+          } else if (this.devoirService.devoir.exercices[indexExe].questions) {
+            for (const [indexQue, question] of this.devoirService.devoir.exercices[indexExe].questions.entries()) {
+              if (question.id === selectedItem.id && selectedPosition === 'before') {
+                // On insère avant l'identifiant fourni
+                this.devoirService.devoir.exercices[indexExe].questions.splice(indexQue, 0, item);
+                break;
+              } else if (question.id === selectedItem.id && selectedPosition === 'end') {
+                // Si on veut ajouter à la fin de la question
+                this.devoirService.devoir.exercices[indexExe].questions[indexQue].criteres.push(item);
+              } else if (this.devoirService.devoir.exercices[indexExe].questions[indexQue].criteres) {
+                for (const [indexCri, critere] of this.devoirService.devoir.exercices[indexExe].questions[indexQue].criteres.entries()) {
+                  if (critere.id === selectedItem.id) {
+                    // Critère de référence trouvé : on mémorise sa position
+                    this.devoirService.devoir.exercices[indexExe].questions[indexQue].criteres.splice(indexCri, 0, item);
+                    break;
+                  }
                 }
               }
             }
           }
         }
       }
+
       // Remove is now finished then display a confirmation
-      this.messageService.add('Suppression terminée', 'success', 'USER');
+      this.messageService.add('Déplacement terminé', 'success', 'USER');
     }, (reason) => {
       // Remove is cancelled
-      this.messageService.add('Suppression annulée', 'warning', 'USER');
+      this.messageService.add('Déplacement annulé', 'warning', 'USER');
     });
   }
 
