832e40784b9acf93d42d524ad15e695323d54cb7
u008039 <hubert.martin-deidier@safrangroup.com>
2021-12-09 14:15:23 +0100

    Améliorations IHM de configuration

--- src/app/_services/configuration.service.ts
+++ src/app/_services/configuration.service.ts
@@ -10,44 +10,47 @@ export class ConfigurationService {
 
   public storageConfigurationPrefix = Configuration.storagePrefix + Configuration.configurationPrefix;
   public configurationItems: any;
+  public categories : string[];
 
   constructor() {
 
+    // Create categories
+    this.categories = ['Général','Devoir','Exercices','Questions','Critères','Textes Libres','Notation','Impression','Avancée' ]
     // Create configuration and init it from global configuration
     this.configurationItems = {
       storagePrefix: { modifiable: false, value: Configuration.storagePrefix },
-
       noteStatusOk: { modifiable: false, value: Configuration.noteStatusOk },
       noteStatusEnCours: { modifiable: false, value: Configuration.noteStatusEnCours },
       noteStatusKo: { modifiable: false, value: Configuration.noteStatusKo },
-      noteStatusOkCoeff: { modifiable: true, title: 'Coefficient critère acquis', value: Configuration.noteStatusOkCoeff },
-      noteStatusEnCoursCoeff: { modifiable: true, title: 'Coefficient critère en cours', value: Configuration.noteStatusEnCoursCoeff },
-      noteStatusKoCoeff: { modifiable: true, title: 'Coefficient critère non acquis', value: Configuration.noteStatusKoCoeff },
+      noteStatusOkCoeff: { modifiable: true, title: 'Coefficient critère acquis', value: Configuration.noteStatusOkCoeff, categorie: 'Notation' },
+      noteStatusEnCoursCoeff: { modifiable: true, title: 'Coefficient critère en cours', value: Configuration.noteStatusEnCoursCoeff, categorie: 'Notation' },
+      noteStatusKoCoeff: { modifiable: true, title: 'Coefficient critère non acquis', value: Configuration.noteStatusKoCoeff, categorie: 'Notation' },
       notationModeNormal: { modifiable: false, value: Configuration.notationModeNormal },
       notationModeProportionnel: { modifiable: false, value: Configuration.notationModeProportionnel },
       notationModeRapporte: { modifiable: false, value: Configuration.notationModeRapporte },
       notationModeArr: { modifiable: false, value: Configuration.notationModeArr },
+      messageDuration: { modifiable: true, title: 'Durée affichage messages', value: Configuration.messageDuration, categorie: 'Avancée' },
+      autosaveDuration: { modifiable: true, title: 'Enregistrement automatique (secondes)', value: Configuration.autosaveDuration, categorie: 'Avancée' },
+      defaultMode: { modifiable: true, title: 'Mode par défaut [edition,notation]', value: Configuration.defaultMode, categorie: 'Général' },
+      notationDefaultOrder: { modifiable: true, title: 'Ordre affichage par défaut [nom,note]', value: Configuration.notationDefaultOrder, categorie: 'Général' },
+      notationDefaultNoteMode: { modifiable: true, title: 'Affichage des notes par défaut [brut,ajuste]', value: Configuration.notationDefaultNoteMode, categorie: 'Général' },
+      author: { modifiable: true, title: 'Auteur par défaut', value: Configuration.author, categorie: 'Devoir'  },
+      devoirTitreDefault: { modifiable: true, title: 'Titre par défaut', value: Configuration.devoirTitreDefault, categorie: 'Devoir' },
+      devoirArrondiDefault: { modifiable: true, title: 'Arrondi par défaut', value: Configuration.devoirArrondiDefault, categorie: 'Devoir' },
+      devoirNotationModeDefault: { modifiable: true, title: 'Mode par défaut [1=Norm,2=Prop,3=Rapp]', value: Configuration.devoirNotationModeDefault, categorie: 'Devoir' },
+      devoirNotationCibleDefault: { modifiable: true, title: 'Note cible par défaut', value: Configuration.devoirNotationCibleDefault, categorie: 'Devoir' },
+      exerciceTitreDefaut: { modifiable: true, title: 'Titre par défaut', value: Configuration.exerciceTitreDefaut, categorie: 'Exercices' },
+      exerciceNbQuestionDefaut: { modifiable: true, title: 'Nb questions par défaut', value: Configuration.exerciceNbQuestionDefaut, categorie: 'Exercices' },
+      questionTitreDefaut: { modifiable: true, title: 'Titre par défaut', value: Configuration.questionTitreDefaut, categorie: 'Questions' },
+      questionNbFreetextDefaut: { modifiable: true, title: 'Nb texte par défaut', value: Configuration.questionNbFreetextDefaut, categorie: 'Questions' },
+      questionNbCritereDefaut: { modifiable: true, title: 'Nb critères par défaut', value: Configuration.questionNbCritereDefaut, categorie: 'Questions' },
+      critereTitreDefaut: { modifiable: true, title: 'Titre par défaut', value: Configuration.critereTitreDefaut, categorie: 'Critères' },
+      critereBaremeDefaut: { modifiable: true, title: 'Barème par défaut', value: Configuration.critereBaremeDefaut, categorie: 'Critères' },
+      critereFreeDefaut: { modifiable: true, title: 'Texte par défaut', value: Configuration.critereFreeDefaut, categorie: 'Textes Libres' },
+      groupeNomDefaut: { modifiable: true, title: 'Nom de groupe par défaut', value: Configuration.groupeNomDefaut, categorie: 'Notation' },
+      impressionMargeExercice: { modifiable: true, title: 'Espace avant exercice', value: Configuration.impressionMargeExercice, categorie: 'Impression' },
+      impressionMargeQuestion: { modifiable: true, title: 'Espace avant question', value: Configuration.impressionMargeQuestion, categorie: 'Impression' }
 
-      messageDuration: { modifiable: true, title: 'Durée affichage messages', value: Configuration.messageDuration },
-      autosaveDuration: { modifiable: true, title: 'Enregistrement automatique (secondes)', value: Configuration.autosaveDuration },
-      defaultMode: { modifiable: true, title: 'Mode par défaut [edition,notation]', value: Configuration.defaultMode },
-      notationDefaultOrder: { modifiable: true, title: 'Ordre affichage par défaut [nom,note]', value: Configuration.notationDefaultOrder },
-      notationDefaultNoteMode: { modifiable: true, title: 'Affichage des notes par défaut [brut,ajuste]', value: Configuration.notationDefaultNoteMode },
-
-      author: { modifiable: true, title: 'Auteur des devoirs', value: Configuration.author },
-      devoirTitreDefault: { modifiable: true, title: 'Devoir / Titre par défaut', value: Configuration.devoirTitreDefault },
-      devoirArrondiDefault: { modifiable: true, title: 'Devoir / Arrondi par défaut', value: Configuration.devoirArrondiDefault },
-      devoirNotationModeDefault: { modifiable: true, title: 'Devoir / Mode par défaut', value: Configuration.devoirNotationModeDefault },
-      devoirNotationCibleDefault: { modifiable: true, title: 'Devoir / Cible par défaut', value: Configuration.devoirNotationCibleDefault },
-      exerciceTitreDefaut: { modifiable: true, title: 'Exercice / Titre par défaut', value: Configuration.exerciceTitreDefaut },
-      exerciceNbQuestionDefaut: { modifiable: true, title: 'Exercice / Nb questions par défaut', value: Configuration.exerciceNbQuestionDefaut },
-      questionTitreDefaut: { modifiable: true, title: 'Question / Titre par défaut', value: Configuration.questionTitreDefaut },
-      questionNbFreetextDefaut: { modifiable: true, title: 'Question / Nb texte par défaut', value: Configuration.questionNbFreetextDefaut },
-      questionNbCritereDefaut: { modifiable: true, title: 'Question / Nb critères par défaut', value: Configuration.questionNbCritereDefaut },
-      critereTitreDefaut: { modifiable: true, title: 'Critère / Titre par défaut', value: Configuration.critereTitreDefaut },
-      critereBaremeDefaut: { modifiable: true, title: 'Critère / Barème par défaut', value: Configuration.critereBaremeDefaut },
-      critereFreeDefaut: { modifiable: true, title: 'Texte libre / Texte par défaut', value: Configuration.critereFreeDefaut },
-      groupeNomDefaut: { modifiable: true, title: 'Nom de groupe par défaut', value: Configuration.groupeNomDefaut }
     };
 
     // Load local storage that replace the global initial storage
